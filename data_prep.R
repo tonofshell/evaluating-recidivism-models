e549d542da8df3a4430aa8b02a06ef07bd8394d6
Adam Shelton <aqs@uchicago.edu>
2020-06-04 22:25:09 -0500

    Added new dataset and code to clean and process

--- data_prep.R
+++ data_prep.R
@@ -3,33 +3,24 @@
 library(tidyverse)
 library(cowpoke)
 library(lubridate)
+library(haven)
 library(here)
 
-raw_data = read_csv(here("compas-scores-two-years.csv"))
-
-can_be_logical = function(x) {
-  x = as.character(x) %>% str_to_lower()
-  unique(x) %in% c("0", "1", "t", "f", "true", "false") %>% all()
+rep_nums = function(num, times_seq) {
+  sapply(times_seq, (function(x) rep(num, x) %>% paste(collapse = "") %>% as.numeric()))
 }
 
-analysis_data = raw_data %>% 
-  select(c(id, sex, age, age_cat, race, juv_fel_count, juv_misd_count, juv_other_count, priors_count, days_b_screening_arrest, c_offense_date, c_charge_degree, c_charge_desc, is_recid, is_violent_recid, decile_score_1, score_text, v_decile_score, v_score_text, two_year_recid)) %>% 
-  filter(days_b_screening_arrest <= 30) %>%
-  filter(days_b_screening_arrest >= -30) %>%
-  select(-days_b_screening_arrest) %>% 
-  filter(is_recid != -1) %>%
-  filter(c_charge_degree != "O") %>%
-  filter(score_text != 'N/A') %>% 
-  mutate(c_offense_date = ymd(c_offense_date), 
-         offense_year = year(c_offense_date), 
-         offense_month = month(c_offense_date), 
-         offense_day_week = wday(c_offense_date), 
-         offense_day_month = day(c_offense_date), 
-         offense_day_year = yday(c_offense_date),
-         age_cat = factor(age_cat, c("Less than 25", "25 - 45", "Greater than 45"), ordered = TRUE),
-         score_text = factor(score_text, c("Low", "Medium", "High"), ordered = TRUE), 
-         v_score_text = factor(v_score_text, c("Low", "Medium", "High"), ordered = TRUE)) %>% 
-  mutate_if(is.character, factor) %>% 
-  mutate_if(can_be_logical, as.logical)
+fix_num_nas = function(vect) {
+  max_digits = suppressWarnings(vect %>% unique() %>% as.numeric() %>% max() %>% (function(x) floor(log10(x)) + 1))
+  if (is.finite(max_digits) & max_digits > 0) {
+    na_vals = vect %in% c(rep_nums(9, max_digits), rep_nums(8, max_digits))
+    vect[na_vals] = NA
+  }
+  vect
+}
 
-saveRDS(analysis_data, here("cleaned_data.rds"))
+raw_data = read_dta(here("us_pretrial_release_data.dta"))
+
+processed_data = raw_data %>% mutate_all(fix_num_nas) %>% mutate_if(is.labelled, as_factor) %>% mutate_all(fix_nas, na_strs = c("", " ", ".", "Not applicable", "Blank", "-9"))
+
+saveRDS(processed_data, here("cleaned_data.rds"))
